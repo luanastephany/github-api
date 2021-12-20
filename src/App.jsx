f14a0beacb30201878b29084d418fb23ecb7df6d
Luana Stephany <ls.luanastephany@gmail.com>
2021-12-20 18:16:27 -0300

    atualizando e criando service api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Layout from "./components/Layout";
+import Profile from "./components/Profile";
+
 function App() {
-  return <div className="App">github</div>;
+  return (
+    <main>
+      <Layout>
+        <Profile />
+
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
+  );
 }
 
 export default App;
