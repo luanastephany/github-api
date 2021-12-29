982b07f2181527264fd65d0c0d6792807d5e1378
Luana Stephany <ls.luanastephany@gmail.com>
2021-12-29 09:51:01 -0300

    updating layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "../Header";
 import { WrapperLayout } from "./styled";
 
 const Layout = ({ children }) => {
   return (
     <WrapperLayout>
-      <header>Header</header>
+      <Header />
       {children}
     </WrapperLayout>
   );
